d105f81b46d162ab4954a94152f5b6e23ad2b3f9
Ygal Blum <ygal.blum@gmail.com>
2026-02-23 11:45:38 -0500

    Use simple strings with newline instead of multiline strings
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- internal/rego/parser_test.go
+++ internal/rego/parser_test.go
@@ -15,82 +15,49 @@ func TestRego(t *testing.T) {
 var _ = Describe("ExtractPackageName", func() {
 	Context("Valid package declarations", func() {
 		It("should extract simple package name", func() {
-			regoCode := `package test
-
-allow {
-    true
-}`
+			regoCode := "package test\n\nallow {\n    true\n}"
 			pkgName, err := ExtractPackageName(regoCode)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(pkgName).To(Equal("test"))
 		})
 
 		It("should extract namespaced package name", func() {
-			regoCode := `package policies.my_policy
-
-allow {
-    true
-}`
+			regoCode := "package policies.my_policy\n\nallow {\n    true\n}"
 			pkgName, err := ExtractPackageName(regoCode)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(pkgName).To(Equal("policies.my_policy"))
 		})
 
 		It("should extract multi-level namespaced package", func() {
-			regoCode := `package a.b.c
-
-allow {
-    true
-}`
+			regoCode := "package a.b.c\n\nallow {\n    true\n}"
 			pkgName, err := ExtractPackageName(regoCode)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(pkgName).To(Equal("a.b.c"))
 		})
 
 		It("should handle package with trailing comment", func() {
-			regoCode := `package test # this is a comment
-
-allow {
-    true
-}`
+			regoCode := "package test # this is a comment\n\nallow {\n    true\n}"
 			pkgName, err := ExtractPackageName(regoCode)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(pkgName).To(Equal("test"))
 		})
 
 		It("should handle package with extra whitespace", func() {
-			regoCode := `package   test
-
-allow {
-    true
-}`
+			regoCode := "package   test\n\nallow {\n    true\n}"
 			pkgName, err := ExtractPackageName(regoCode)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(pkgName).To(Equal("test"))
 		})
 
 		It("should handle package declaration after empty lines", func() {
-			regoCode := `
-
-package test
-
-allow {
-    true
-}`
+			regoCode := "\n\npackage test\n\nallow {\n    true\n}"
 			pkgName, err := ExtractPackageName(regoCode)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(pkgName).To(Equal("test"))
 		})
 
 		It("should handle package declaration after comment lines", func() {
-			regoCode := `# This is a header comment
-# Another comment line
-
-package test
-
-allow {
-    true
-}`
+			regoCode := "# This is a header comment\n# Another comment line\n\npackage test\n\nallow {\n    true\n}"
 			pkgName, err := ExtractPackageName(regoCode)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(pkgName).To(Equal("test"))
@@ -99,46 +66,35 @@ allow {
 
 	Context("Invalid package declarations", func() {
 		It("should return error when no package declaration found", func() {
-			regoCode := `allow {
-    true
-}`
+			regoCode := "allow {\n    true\n}"
 			_, err := ExtractPackageName(regoCode)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("no package declaration found"))
 		})
 
 		It("should return error when package declaration is empty", func() {
-			regoCode := `package
-
-allow {
-    true
-}`
+			regoCode := "package\n\nallow {\n    true\n}"
 			_, err := ExtractPackageName(regoCode)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("empty package declaration"))
 		})
 
 		It("should return error when package declaration is only whitespace", func() {
-			regoCode := `package
-
-allow {
-    true
-}`
+			regoCode := "package\n\nallow {\n    true\n}"
 			_, err := ExtractPackageName(regoCode)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("empty package declaration"))
 		})
 
 		It("should return error when only comments exist", func() {
-			regoCode := `# Just comments
-# No package declaration`
+			regoCode := "# Just comments\n# No package declaration"
 			_, err := ExtractPackageName(regoCode)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("no package declaration found"))
 		})
 
 		It("should return error when empty Rego code", func() {
-			regoCode := ``
+			regoCode := ""
 			_, err := ExtractPackageName(regoCode)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("no package declaration found"))
